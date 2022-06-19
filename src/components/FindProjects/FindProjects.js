eaa9e925b4f865944e8aafea1664b2263839b82b
imnaeem <ceinfo.pk@gmail.com>
2022-06-20 00:54:17 +0500

    Yes

--- src/components/FindProjects/FindProjects.js
+++ src/components/FindProjects/FindProjects.js
@@ -139,81 +139,86 @@ const FindProjects = () => {
             )}
 
           {JSON.parse(localStorage.getItem("profile")) &&
-          JSON.parse(localStorage.getItem("profile")).result.type ===
-            "company" ? (
-            <Box>
-              {company.fetched ? (
-                <Box>
-                  {company.company.username !== "" ? (
-                    <Box>
-                      {allProjects.fetched ? (
-                        <Box>
-                          {projects.length > 0 ? (
-                            <Stack direction="column" spacing={3}>
-                              {perPageProjects.map((project, index) => (
-                                <LazyLoad key={index} height={300}>
-                                  <Projects project={project} />
-                                </LazyLoad>
-                              ))}
-                              <Stack direction="row" justifyContent="center">
-                                <Pagination
-                                  count={count}
-                                  size="large"
-                                  page={page}
-                                  variant="outlined"
-                                  shape="rounded"
-                                  onChange={handleChange}
-                                />
-                              </Stack>
-                            </Stack>
-                          ) : (
-                            <Box
-                              sx={{ padding: "10px 10px", textAlign: "center" }}
-                            >
-                              <Typography>No Projects Found</Typography>
-                            </Box>
-                          )}
-                        </Box>
-                      ) : (
-                        <Box sx={{ padding: "10px 10px" }}>
-                          <LinearProgress />
-                        </Box>
-                      )}
-                    </Box>
-                  ) : (
-                    <Stack
-                      direction="column"
-                      spacing={2}
-                      justifyContent="center"
-                      alignItems="center"
-                    >
-                      <Alert
-                        elevation={1}
-                        severity="warning"
-                        sx={{ alignItems: "center" }}
-                      >
-                        Please Complete Your Company Profile to View Projects
-                      </Alert>
+            JSON.parse(localStorage.getItem("profile")).result.type ===
+              "company" && (
+              <Box>
+                {company.fetched ? (
+                  <Box>
+                    {company.company.username !== "" ? (
                       <Box>
-                        <Button
-                          componenet="a"
-                          href="company/edit-company"
-                          size="large"
-                          variant="contained"
-                        >
-                          Complete Profile
-                        </Button>
+                        {allProjects.fetched ? (
+                          <Box>
+                            {projects.length > 0 ? (
+                              <Stack direction="column" spacing={3}>
+                                {perPageProjects.map((project, index) => (
+                                  <LazyLoad key={index} height={300}>
+                                    <Projects project={project} />
+                                  </LazyLoad>
+                                ))}
+                                <Stack direction="row" justifyContent="center">
+                                  <Pagination
+                                    count={count}
+                                    size="large"
+                                    page={page}
+                                    variant="outlined"
+                                    shape="rounded"
+                                    onChange={handleChange}
+                                  />
+                                </Stack>
+                              </Stack>
+                            ) : (
+                              <Box
+                                sx={{
+                                  padding: "10px 10px",
+                                  textAlign: "center",
+                                }}
+                              >
+                                <Typography>No Projects Found</Typography>
+                              </Box>
+                            )}
+                          </Box>
+                        ) : (
+                          <Box sx={{ padding: "10px 10px" }}>
+                            <LinearProgress />
+                          </Box>
+                        )}
                       </Box>
-                    </Stack>
-                  )}
-                </Box>
-              ) : (
-                <Box sx={{ padding: "10px 10px" }}>
-                  <LinearProgress />
-                </Box>
-              )}
-            </Box>
-          ) : (
+                    ) : (
+                      <Stack
+                        direction="column"
+                        spacing={2}
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Alert
+                          elevation={1}
+                          severity="warning"
+                          sx={{ alignItems: "center" }}
+                        >
+                          Please Complete Your Company Profile to View Projects
+                        </Alert>
+                        <Box>
+                          <Button
+                            componenet="a"
+                            href="company/edit-company"
+                            size="large"
+                            variant="contained"
+                          >
+                            Complete Profile
+                          </Button>
+                        </Box>
+                      </Stack>
+                    )}
+                  </Box>
+                ) : (
+                  <Box sx={{ padding: "10px 10px" }}>
+                    <LinearProgress />
+                  </Box>
+                )}
+              </Box>
+            )}
+
+          {!JSON.parse(localStorage.getItem("profile")) && (
             <Box>
               {allProjects.fetched ? (
                 <Box>
