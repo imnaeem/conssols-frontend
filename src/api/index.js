3db981c5596c3140c8cb2d9945b7f9726db3d062
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 18:52:43 +0500

    APIs url change

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000",
+  baseURL: "https://conssols.herokuapp.com" || "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
