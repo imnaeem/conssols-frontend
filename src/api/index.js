438a162bac9ef24d1f11c5afea7272659768ed36
imnaeem <ceinfo.pk@gmail.com>
2022-06-19 20:30:22 +0500

    APIs url change

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.BASE_URL || "http://localhost:5000",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
